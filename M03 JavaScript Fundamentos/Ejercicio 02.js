554abc3cd19c5f0972456738958023e167dfe36d
charli <karlochug@gmail.com>
2023-09-20 17:49:30 -0600

    mi segunda  tarea

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,10 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   var nombre = "carlos";
+
+   return nombre;
+
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,26 +15,40 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   var sum = x + y;
+   return sum ;
+
+
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   var rest = x - y;
+   return rest;
+
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+
+   var div = x / y;
+   return div;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   var multi= x * y;
+   return multi;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   var residuo = x % y;
+   return residuo;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
