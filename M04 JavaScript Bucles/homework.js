2c6e1fb74c88b3e139f035c518faf950d06ecda3
charli <karlochug@gmail.com>
2023-09-22 01:58:07 -0600

    Tarea cuatro correccion

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -169,18 +169,22 @@ function operadoresLogicos(num1, num2, num3) {
    // Si todos los argumentos son cero, retornar ---> "Error".
    // Si no se cumple ninguna de las condiciones anteriores, retornar false.
    // Tu código:
-   if((num1 > num2) && (num1 > num3) && (num1 > 0 )){
-      return ("Numero 1 es mayor y positivo");
-  }else if((num1 < 0) || (num2 < 0) || (num3 < 0 )){
-          return("Hay negativos");
-      }else if((num3 > num2) && (num3 > num1)){
-          num3++;
-          return num3;
-      } else if((num1 == 0) && (num2 == 0) && (num3 == 0 )){
-          return("Error");
-      }else {
-          return false;
-      }
+   if (num1 < 0) {
+      return("Hay negativos");
+  } else  if (num2 < 0) {
+      return("Hay negativos");
+  } else  if (num3 < 0) {
+      return("Hay negativos");
+  } else if((num1 > num2) && (num1 > num3) && (num1 > 0 )){
+     return ("Numero 1 es mayor y positivo");
+ }else  if((num3 > num2) && (num3 > num1)){
+         num3++;
+         return num3;
+     } else if((num1 == 0) && (num2 == 0) && (num3 == 0 )){
+         return("Error");
+     }else {
+         return false;
+     }
 }
 
 function esPrimo(num) {
