d2ffdb4effdb6c620d5e12d80ebe2b2e1a642bcf
charli <karlochug@gmail.com>
2023-09-22 20:03:57 -0600

    Tarea 05

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -32,7 +32,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
-   return array.push(elemento);
+    array.push(elemento);
+    return (array);
+
 
 }
 
